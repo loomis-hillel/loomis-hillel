afd366517fff90a11917360f83a4076f4637e1f2
loomis-hillel <loomis@ehillel.org>
2021-11-29 09:54:23 +0000

    update datatypes

--- unit7/datatypes.c
+++ unit7/datatypes.c
@@ -3,26 +3,55 @@
 #include <limits.h>
 #include <float.h>
 
+void printBar( int size ){
+    for (int i=0; i<size; i++ )
+        printf( "-" );
+    printf ( "\n" );
+}
+
 int main(void) {
 
-    printf("char:");
-    printf("\tbits :\t%d\n", CHAR_BIT);
-    printf("\tmin  :\t%d\n", CHAR_MIN);
-    printf("\tmax  :\t%d\n", CHAR_MAX);
+    printf("char:\n");
+    printBar(48);
+    printf("\tbytes :\t%d\n", CHAR_BIT/8);
+    printf("\tmin   :\t%d\n", CHAR_MIN);
+    printf("\tmax   :\t%d\n", CHAR_MAX);
+    printBar(48);
 
+    printf("short:\n");
+    printBar(48);
+    printf("\tbytes :\t%lu\n", sizeof(short));
+    printf("\tmin   :\t%d\n", SHRT_MIN);
+    printf("\tmax   :\t%d\n", SHRT_MAX);
+    printBar(48);
 
-    printf("int min      :\t%d\n", INT_MAX);
-    printf("INT_MIN     :   %d\n", INT_MIN);
-    printf("LONG_MAX    :   %ld\n", (long) LONG_MAX);
-    printf("LONG_MIN    :   %ld\n", (long) LONG_MIN);
-    printf("SCHAR_MAX   :   %d\n", SCHAR_MAX);
-    printf("SCHAR_MIN   :   %d\n", SCHAR_MIN);
-    printf("SHRT_MAX    :   %d\n", SHRT_MAX);
-    printf("SHRT_MIN    :   %d\n", SHRT_MIN);
-    printf("UCHAR_MAX   :   %d\n", UCHAR_MAX);
-    printf("UINT_MAX    :   %u\n", (unsigned int) UINT_MAX);
-    printf("ULONG_MAX   :   %lu\n", (unsigned long) ULONG_MAX);
-    printf("USHRT_MAX   :   %d\n", (unsigned short) USHRT_MAX);
+    printf("int:\n");
+    printBar(48);
+    printf("\tbytes :\t%lu\n", sizeof(int));
+    printf("\tmin   :\t%d\n", INT_MIN);
+    printf("\tmax   :\t%d\n", INT_MAX);
+    printBar(48);
+
+    printf("unsigned int:\n");
+    printBar(48);
+    printf("\tbytes :\t%lu\n", sizeof(unsigned int));
+    printf("\tmin   :\t%u\n", 0);
+    printf("\tmax   :\t%u\n", UINT_MAX);
+    printBar(48);
+
+    printf("long:\n");
+    printBar(48);
+    printf("\tbytes :\t%lu\n", sizeof(long));
+    printf("\tmin   :\t%li\n", LONG_MIN);
+    printf("\tmax   :\t%li\n", LONG_MAX);
+    printBar(48);
+
+    printf("long:\n");
+    printBar(48);
+    printf("\tbytes :\t%lu\n", sizeof(unsigned long));
+    printf("\tmin   :\t%u\n", 0);
+    printf("\tmax   :\t%lu\n", ULONG_MAX);
+    printBar(48);
 
     return 0;
 }
