aeb31bcc3e06bc5abd8c8587a61b26f6afdeb674
loomis-hillel <loomis@ehillel.org>
2021-12-02 20:27:02 +0000

    update cash

---
+++ unit7/cash.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <cs50.h>
+
+/**
+ * This program asks the user how much change
+ * they should return in US currency to a customer.
+ * It determines the amount of change (i.e. the amount of
+ * each coin) to return to the customer. This assumes
+ * that the only currency available for change is quarters,
+ * dimes, nickels, and pennies.
+ * Author: your name here
+ */
+int main(void) {
+
+    // Declare your variables below.
+
+    // Collect the user input as a decimal number
+
+    // Provide output on how much change to return to
+    // the user below.
+
+    return 0;
+}
